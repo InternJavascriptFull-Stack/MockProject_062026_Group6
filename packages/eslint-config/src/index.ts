468052b51318554e9e1bacc315b9bec2fb020bff
ThanhVan83 <zanthanh8324@gmail.com>
2026-06-30 11:47:40 +0700

    Revert "Feature/patient hovanthanh"

--- packages/eslint-config/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as baseConfig } from "./base.js";
-export { default as reactConfig } from "./react.js";
-export { default as architectureConfig } from "./architecture.js";
-export { default as backendConfig } from "./backend.js";
-export { defineConfig, globalIgnores } from "eslint/config";
